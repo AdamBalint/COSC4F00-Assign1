e7a501919f11b9ce0e39f35732d4157013cbd4ef
Adam Balint <ab11fl@brocku.ca>
2016-10-06 10:47:32 -0400

    Initial Commit

---
+++ Assign_1/ADT.cpp
@@ -0,0 +1,9 @@
+//
+//  ADT.cpp
+//  Assign_1
+//
+//  Created by Adam Balint on 2016-10-06.
+//  Copyright © 2016 Adam Balint. All rights reserved.
+//
+
+#include "ADT.hpp"
---
+++ Assign_1/ADT.hpp
@@ -0,0 +1,14 @@
+//
+//  ADT.hpp
+//  Assign_1
+//
+//  Created by Adam Balint on 2016-10-06.
+//  Copyright © 2016 Adam Balint. All rights reserved.
+//
+
+#ifndef ADT_hpp
+#define ADT_hpp
+
+#include <stdio.h>
+
+#endif /* ADT_hpp */
